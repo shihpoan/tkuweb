963481eaec9c4a53ef7ff1d956a7aeb1e618be18
施柏安 <randypunk123@gmail.com>
2023-11-24 13:49:22 +0800

    ok

--- app/(students)/academicGuides/[[...courseId]]/page.js
+++ app/(students)/academicGuides/[[...courseId]]/page.js
@@ -26,11 +26,11 @@ function page({ params }) {
 
   return (
     <>
-      <div className="flex flex-col w-screen h-screen bg-white overflow-auto">
+      <div className="flex flex-col w-screen h-screen bg-white justify-between items-stretch overflow-auto">
         <div className="flex w-[100%] min-h-[15%] bg-white text-primary_500 text-3xl justify-center items-center">
           課程標題
         </div>
-        <div className="flex flex-col w-[100%] min-h-[70%] h-max bg-gray-200">
+        <div className="flex flex-col w-[100%] h-max bg-gray-200">
           <Image
             priority
             width={400}
@@ -38,16 +38,16 @@ function page({ params }) {
             src={"/images/test.png"}
             alt="test"
           />
-          <div id="content" className="flex flex-col w-[100%] h-max">
+          <div id="contents" className="flex flex-col flex-grow w-[100%]">
             <div id="tag" className="w-[100%] h-max p-2">
-              <div className="w-max h-max text-white bg-primary_500 rounded p-2">
+              <div className="flex w-max h-max text-white bg-primary_500 rounded p-2">
                 庄子后溪
               </div>
-              <div className="w-[100%] h-max text-lg text-black px-4 py-2">
-                <p>
-                  庄子內溪，為淡水河支流之一，以前英專路一帶稱為庄仔內，溪流經過庄仔內故名。在庄仔內溪河口、外鼻仔頭的西側，淡水捷運站、殼牌倉庫、水上人家旁，又稱「港仔溝」、「港仔溝海」，因庄子內溪下游段溪水流入淡水港形成的港灣而得名。而庄仔內溪也流經淡水學府里與鄧公里，是貫穿這兩個里的重要溪流，其沿線還有已廢棄的雙峻頭南一圳、早期軍方設施，同時因為廢棄使用，使得生態相當豐富。
-                </p>
-              </div>
+            </div>
+            <div className="flex w-[100%] h-max text-lg text-black px-4 py-2">
+              <p>
+                庄子內溪，為淡水河支流之一，以前英專路一帶稱為庄仔內，溪流經過庄仔內故名。在庄仔內溪河口、外鼻仔頭的西側，淡水捷運站、殼牌倉庫、水上人家旁，又稱「港仔溝」、「港仔溝海」，因庄子內溪下游段溪水流入淡水港形成的港灣而得名。而庄仔內溪也流經淡水學府里與鄧公里，是貫穿這兩個里的重要溪流，其沿線還有已廢棄的雙峻頭南一圳、早期軍方設施，同時因為廢棄使用，使得生態相當豐富。
+              </p>
             </div>
           </div>
         </div>
