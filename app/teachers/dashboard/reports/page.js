f7b0acc4a2344569d218526eaab9ddd1732b2fa0
施柏安 <randypunk123@gmail.com>
2023-12-26 17:03:29 +0800

    ok

--- app/teachers/dashboard/reports/page.js
+++ app/teachers/dashboard/reports/page.js
@@ -1,36 +1,27 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation.js";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
-function createData(id, name, date, course) {
-  return { id, name, date, course };
-}
-
-const rows = [
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-  createData("1234567", "施水水", "2023.11.29", "庄子後溪"),
-];
+import { DataGrid } from "@mui/x-data-grid";
 
 import { useNodePostApi } from "@/hooks/useNodeApi.js";
 
-export default function Page() {
+function createData(id, student_id, name, _class, report, score) {
+  return { id, student_id, name, _class, report, score };
+}
+
+export default function BasicTable() {
   const router = useRouter();
 
   const [ccsoList, setCcsoList] = useState([]);
   const [cslrList, setCslrList] = useState([]);
   const [rrList, setRrList] = useState([]);
   const [slList, setSlList] = useState([]);
+
+  const [mixDatas, setMixDatas] = useState([]);
+
+  const [rows, setRows] = useState([]);
+
   useEffect(() => {
     async function findRecords() {
       try {
@@ -47,10 +38,10 @@ export default function Page() {
         setCslrList([...Cslr]);
         setRrList([...Rr]);
         setSlList([...Sl]);
-        // console.log("Ccso", Ccso);
-        // console.log("Cslr", Cslr);
-        // console.log("Rr", Rr);
-        // console.log("Sl", Sl);
+        console.log("Ccso", Ccso);
+        console.log("Cslr", Cslr);
+        console.log("Rr", Rr);
+        console.log("Sl", Sl);
         // if (dbRivers.statusText != "OK") {
         //   throw new Error("Failed to fetch data");
         // }
@@ -61,6 +52,63 @@ export default function Page() {
     findRecords();
   }, []);
 
+  useEffect(() => {
+    const _mixArray = [...ccsoList, ...cslrList, ...rrList, ...slList];
+    console.log("sss", _mixArray);
+    const dataReducer = _mixArray.reduce((acc, curr) => {
+      const { _id, student_id, student_name, reportName, score } = curr;
+      const _class = curr.class;
+      const returnDatas = {
+        _id,
+        student_id,
+        student_name,
+        _class,
+        reportName,
+        score,
+      };
+
+      acc.push(returnDatas);
+
+      return acc;
+    }, []);
+
+    console.log("dataReducer", dataReducer);
+
+    setMixDatas([...dataReducer]);
+  }, [ccsoList, cslrList, rrList, slList]);
+
+  useEffect(() => {
+    const _row = mixDatas.map((d) =>
+      createData(
+        d._id,
+        d.student_id,
+        d.student_name,
+        d._class,
+        d.reportName,
+        d.score
+      )
+    );
+    setRows([..._row]);
+  }, [mixDatas]);
+
+  const columns = [
+    // { field: "id", headerName: "ID", width: 150 },
+    { field: "student_id", headerName: "學號", width: 150 },
+    { field: "name", headerName: "姓名", width: 150 },
+    { field: "_class", headerName: "班級", width: 150 },
+    {
+      field: "report",
+      headerName: "問卷名稱",
+      width: 150,
+    },
+    {
+      field: "score",
+      headerName: "分數",
+      sortable: false,
+      width: 150,
+    },
+  ];
+
   return (
     <main className="flex flex-col h-[100%] justify-between items-center py-8 overflow-x-hidden">
       <div className="flex flex-col text-4xl text-black text-center font-bold gap-8">
@@ -68,107 +116,49 @@ export default function Page() {
           <p>112學年淡江大學</p>
           <p className="text-xl">校園與社區服務學習課程服務成果</p>
         </div>
-        {/* <div className="flex flex-col justify-start items-start gap-4">
-          <p className="text-xl">CCso</p>
-          {ccsoList.map((record, rIdx) => (
-            <div key={rIdx} className="flex text-base gap-2 items-center">
-              <p>{rIdx + 1}.</p>
-              <div
-                className="flex w-max max-w-screen text-base text-gray-800 border-[2px] border-black bg-white rounded p-2 gap-4"
-                onClick={() =>
-                  router.push(`/dashboard/editRiver/${record._id.toString()}`)
-                }
-              >
-                <p>{record.student_id}</p>
-                <p>{record.student_name}</p>
-                <p>{record.date}</p>
-              </div>
-            </div>
-          ))}
-          <p className="text-xl">Cslr</p>
-          {cslrList.map((record, rIdx) => (
-            <div key={rIdx} className="flex text-base gap-2 items-center">
-              <p>{rIdx + 1}.</p>
-              <div
-                className="flex w-max text-base text-gray-800 border-[2px] border-black bg-white rounded p-2 gap-4"
-                onClick={() =>
-                  router.push(`/dashboard/editRiver/${record._id.toString()}`)
-                }
-              >
-                <p>{record.student_id}</p>
-                <p>{record.student_name}</p>
-                <p>{record.date}</p>
-              </div>
-            </div>
-          ))}
-          <p className="text-xl">Rr</p>
-          {rrList.map((record, rIdx) => (
-            <div key={rIdx} className="flex text-base gap-2 items-center">
-              <p>{rIdx + 1}.</p>
-              <div
-                className="flex w-max text-base text-gray-800 border-[2px] border-black bg-white rounded p-2 gap-4"
-                onClick={() =>
-                  router.push(`/dashboard/editRiver/${record._id.toString()}`)
-                }
-              >
-                <p>{record.student_id}</p>
-                <p>{record.student_name}</p>
-                <p>{record.date}</p>
-              </div>
-            </div>
-          ))}
-          <p className="text-xl">Sl</p>
-          {slList.map((record, rIdx) => (
-            <div key={rIdx} className="flex text-base gap-2 items-center">
-              <p>{rIdx + 1}.</p>
-              <div
-                className="flex w-max text-base text-gray-800 border-[2px] border-black bg-white rounded p-2 gap-4"
-                onClick={() =>
-                  router.push(`/dashboard/editRiver/${record._id.toString()}`)
-                }
-              >
-                <p>{record.student_id}</p>
-                <p>{record.student_name}</p>
-                <p>{record.date}</p>
-              </div>
-            </div>
-          ))}
-        </div> */}
-        <TableContainer component={Paper}>
-          <Table
-            sx={{ minWidth: 350, background: "white" }}
-            aria-label="simple table"
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">學號</TableCell>
-                <TableCell align="left">姓名</TableCell>
-                <TableCell align="left">日期</TableCell>
-                <TableCell align="left">課程</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row, rIdx) => (
-                <TableRow
-                  key={rIdx}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="left">{row.id}</TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="left">{row.date}</TableCell>
-                  <TableCell align="left">{row.course}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <div className="flex w-[100%] text-xl justify-start">
+          <select className="w-[15%] p-2">
+            <option>all</option>
+            <option>ccso</option>
+            <option>rr</option>
+            <option>sl</option>
+            <option>cslr</option>
+          </select>
+        </div>
+        <div
+          style={{
+            height: 650,
+            width: "100%",
+            backgroundColor: "white",
+          }}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            sx={{ fontSize: "20px" }}
+            pageSizeOptions={[5, 10, 20]}
+            checkboxSelection
+            onRowSelectionModelChange={(i) => console.log(i)}
+          />
+        </div>
       </div>
-      <div className="flex flex-col text-4xl text-gray-800 font-bold gap-8 py-8">
+      <div className="flex text-4xl text-gray-800 font-bold gap-8 py-8">
         <button
           className="text-4xl font-bold border-[2px] border-gray-800 bg-white rounded p-2"
           onClick={() => router.push("/dashboard/newRiver")}
         >
           下載紀錄
+        </button>
+        <button
+          className="text-4xl font-bold border-[2px] border-gray-800 bg-white rounded p-2"
+          onClick={() => router.push("/dashboard/newRiver")}
+        >
+          開啟紀錄
         </button>
       </div>
     </main>
