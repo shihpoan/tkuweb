168f0b6a40a70c5eb65ad9f1a3fb6c197016c60c
施柏安 <randypunk123@gmail.com>
2024-03-30 18:04:24 +0800

    ok

--- app/login/page.js
+++ app/login/page.js
@@ -107,51 +107,6 @@ function Login() {
     },
   ];
 
-  // 原手刻功能，使用 Mui 原件取代
-  // const [studentNumberFocus, setStudentNumberFocus] = useState(false);
-  // const [studentNameFocus, setStudentNameFocus] = useState(false);
-  // const [studentClassFocus, setStudentClassFocus] = useState(false);
-  // const studentNumberInputRef = useRef(null);
-  // const studentNameInputRef = useRef(null);
-  // const studentClassSelectRef = useRef(null);
-  // useEffect(() => {
-  //   if (studentNumberFocus) {
-  //     studentNumberInputRef.current.focus();
-  //   }
-  // }, [studentNumberFocus]);
-
-  // useEffect(() => {
-  //   if (studentNameFocus) {
-  //     studentNameInputRef.current.focus();
-  //   }
-  // }, [studentNameFocus]);
-  // const handleStudentNameFocus = () => {
-  //   setStudentNameFocus(true);
-  // };
-  // const handleStudentNameBlur = () => {
-  //   if (studentNameFocus && !studentNameInputRef.current.value) {
-  //     setStudentNameFocus(false);
-  //   }
-  // };
-
-  // const handleStudentClassFocus = () => {
-  //   setStudentClassFocus(true);
-  // };
-  // const handleStudentClassBlur = () => {
-  //   if (studentClassFocus && !studentClassSelectRef.current.value) {
-  //     setStudentClassFocus(false);
-  //   }
-  // };
-
-  // const handleStudentNumberFocus = () => {
-  //   setStudentNumberFocus(true);
-  // };
-  // const handleStudentNumberBlur = () => {
-  //   if (studentNumberFocus && !studentNumberInputRef.current.value) {
-  //     setStudentNumberFocus(false);
-  //   }
-  // };
-
   return (
     <>
       <div id="title" className="flex flex-col justify-center items-center">
@@ -169,82 +124,6 @@ function Login() {
         >
           登入錯誤
         </div>
-
-        {/* <div className={`relative mb-4 ${studentNumberFocus ? "focus" : ""}`}>
-          <input
-            type="text"
-            placeholder={studentNumberFocus ? "請輸入你的學號" : " "}
-            onFocus={handleStudentNumberFocus}
-            onBlur={handleStudentNumberBlur}
-            onChange={(e) => handleStudentNumberChange(e)}
-            ref={studentNumberInputRef}
-            className="w-full py-4 px-3 rounded border outline-none"
-          />
-          <label
-            htmlFor="passwordInput"
-            onClick={() => setStudentNumberFocus(true)}
-            className={`absolute transition-all duration-300 ${
-              studentNumberFocus
-                ? "-top-6 left-0 text-sm text-gray-600"
-                : "top-4 left-2"
-            }`}
-          >
-            學號
-          </label>
-        </div>
-        <div className={`relative mb-4 ${studentNameFocus ? "focus" : ""}`}>
-          <input
-            type="text"
-            placeholder={studentNameFocus ? "請輸入你的姓名" : " "}
-            onFocus={handleStudentNameFocus}
-            onBlur={handleStudentNameBlur}
-            onChange={(e) => handleStudentNameChange(e)}
-            ref={studentNameInputRef}
-            className="w-full py-4 px-3 rounded border outline-none"
-          />
-          <label
-            htmlFor="passwordInput"
-            // onClick={() => setPasswordFocus(true)}
-            className={`absolute transition-all duration-300 text-gray-500 ${
-              studentNameFocus
-                ? "-top-6 left-0 text-sm text-gray-600"
-                : "top-4 left-2"
-            }`}
-          >
-            姓名
-          </label>
-        </div>
-        <div className={`relative mb-4 ${studentClassFocus ? "focus" : ""}`}>
-          <select
-            onFocus={handleStudentClassFocus}
-            onBlur={handleStudentClassBlur}
-            onChange={(e) => handleStudentClassChange(e)}
-            ref={studentClassSelectRef}
-            className="w-full py-4 px-3 rounded border outline-none"
-          >
-            {studentClassFocus ? (
-              <>
-                <option>{""}</option>
-                <option>班級1</option>
-                <option>班級2</option>
-                <option>班級3</option>
-              </>
-            ) : (
-              ""
-            )}
-          </select>
-          <label
-            htmlFor="passwordInput"
-            onClick={() => handleStudentClassFocus(true)}
-            className={`absolute transition-all duration-300 text-gray-500 ${
-              studentClassFocus
-                ? "-top-6 left-0 text-sm text-gray-600"
-                : "top-4 left-2"
-            }`}
-          >
-            班級
-          </label>
-        </div> */}
         <FormControl>
           <FormLabel>學號</FormLabel>
           <Input
@@ -273,14 +152,14 @@ function Login() {
             onChange={(e) => handleStudentNameChange(e)}
           />
         </FormControl>
-        <FormControl>
+        {/* <FormControl>
           <FormLabel>班級</FormLabel>
           <Autocomplete
             placeholder="請輸入你的班級"
             options={classes}
             onChange={(e) => handleStudentClassChange(e)}
           />
-        </FormControl>
+        </FormControl> */}
       </div>
       <button
         className="w-[10rem] h-[3rem] border-[2px] border-white text-lg text-white bg-primary_500 hover:bg-primary_200 font-bold py-2 px-4 rounded mt-[4rem]"
